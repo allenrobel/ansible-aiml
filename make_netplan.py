f5a385302abbfee4e73824d23d853d50c5c69203
Allen Robel <arobel@cisco.com>
2023-08-16 15:19:12 -1000

    initial commit

---
+++ make_netplan.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+import ipaddress
+
+def make_netplan(host):
+    return f"""
+# This is the network config written by 'subiquity'
+network:
+    version: 2
+    ethernets:
+        eno1:
+            critical: true
+            dhcp-identifier: mac
+            dhcp4: true
+            nameservers:
+                addresses:
+                -   171.70.168.183
+        eno2:
+            dhcp4: true
+        ens1f0np0:
+            dhcp4: false
+        ens1f1np1:
+            dhcp4: false
+    vlans:
+        vlan{host}:
+            id: {host}
+            link: ens1f0np0
+            addresses:
+            -   192.168.{host}.11/24
+            routes:
+            -   to: 192.168.0.0/16
+                via: 192.168.{host}.1
+                metric: 100
+        vlan{host+100}:
+            id: {host+100}
+            link: ens1f1np1
+            addresses:
+            -   172.16.{host}.11/24
+            routes:
+            -   to: 172.16.0.0/16
+                via: 172.16.{host}.1
+                metric: 100
+"""
+for host in range(148,149):
+    print(make_netplan(host))
